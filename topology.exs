0c47a292f50094e407130fe880eb822a2ce47a25
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-09-29 21:19:59 -0400

    #2. Modify logic for next method for all topologies except 3D and imperfect line.

--- topology.exs
+++ topology.exs
@@ -1,30 +1,74 @@
 defmodule Topology do
   def create(type, numNodes) do
     case type do
-      "full_network"   -> FullNetwork.create_network(numNodes)
-      "3D"             -> ThreeD.create_network(numNodes)
-      "random_2D"      -> Random2D.create_network(numNodes)
-      "torus"          -> Torus.create_network(numNodes)
-      "line"           -> Line.create_network(numNodes)
-      "imperfect_line" -> ImperfectLine.create_network(numNodes)
+      "full"   -> &FullNetwork.next/2
+      "rand2D" -> &TwoD.next_random/2
+      "torus"  -> &TwoD.next_torus/2
+      "line"   -> &Line.next/2
+      "3D"     -> &ThreeD.next/2
+      "imperfect_line" -> &ImperfectLine.next/2
     end
   end
 end
 
 
 defmodule FullNetwork do
-  def create_network(numNodes) do
-    network = Enum.map(1..numNodes, fn _x -> Gossip.start_link(0) end)
-    pid = Enum.random(network)
-    {pid, network, &next/2}
+  def next(curr, network) do
+    neighbors = Enum.filter(network, fn -> Process.alive?(pid) && pid != curr end)
+    case Enum.empty?(neighbors) do
+      true  -> curr
+      false -> Enum.random(neighbors)
+    end
+  end
+end
+
+defmodule TwoD do
+  def next_random({r, c}, network) do
+    neighbors = [{r-1, c} , {r+1, c}, {r, c-1}, {r, c+1}]
+                |> Enum.filter(fn {r, c} -> r >=0 and r < N and c >=0 and c < N end)
+                |> clean(network, {r, c})
+    case Enum.empty?(neighbors) do
+      true  -> {r, c}
+      false -> Enum.random(neighbors)
+    end
   end
 
-  def next(curr, network) do
-    network = Enum.filter(network, fn pid -> Process.alive?(pid) && pid != curr end)
-    case Enum.empty?(network) do
-      false -> Enum.random(network)
-      true  -> curr
+  def next_torus({r, c}, network) do
+    N = length(network)
+    neighbors = [{r-1, c} , {r+1, c}, {r, c-1}, {r, c+1}]
+                |> Enum.map(neighbors, fn {r, c} -> {r % N, c % N})
+                |> clean(network, {r, c})
+    case Enum.empty?(neighbors) do
+      true  -> {r, c}
+      false -> Enum.random(neighbors)
     end
+  end
+
+  defp clean(neighbors, network, {x, y}) do
+    N = trunc(:math.sqrt(length(network)))
+    curr = Enum.at(network, get_index(r, c))
+    |> Enum.map(neighbors, fn {r, c} -> Enum.at(network, r * (N-1) + c) end)
+    |> Enum.into(%{})
+    |> Enum.filter(fn {rc, pid} -> Process.alive?(pid) && pid != curr end)
+    |> Map.keys
+  end
+end
+
+defmodule Line do
+  def next(curr_index, network) do
+    neighbors = clean(network, curr_index)
+    case Enum.empty?(pids) do
+      true  -> curr
+      false -> Enum.random(neighbors)
+    end
+  end
+
+  defp clean(neighbors, network, curr_index) do
+    curr = Enum.at(network, curr_index)
+    Enum.map([curr_index-1, curr_index+1], fn index -> Enum.at(network, index) end)
+    |> Enum.into(%{})
+    |> Enum.filter(fn {index, pid} -> !is_nil(pid) && Process.alive?(pid) && pid != curr end)
+    |> Map.keys
   end
 end
 
@@ -43,59 +87,6 @@ end
 #     end
 #   end
 # end
-
-# defmodule Random2D do
-#   def create_network(numNodes) do
-#     network = Enum.map(1..numNodes, fn _x -> Gossip.start_link(0) end)
-#     pid = Enum.random(children_pids)
-#     {pid, network, next}
-#   end
-  
-#   def next(curr, network) do
-#     network = Enum.filter(network, fn pid -> Process.alive?(pid) && pid != curr end)
-#     case Enum.empty?(network) do
-#       false -> Enum.random(network)
-#       true  -> curr
-#     end
-#   end
-# end
-
-# defmodule Torus do
-#   def create_network(numNodes) do
-#     network = Enum.map(1..numNodes, fn _x -> Gossip.start_link(0) end)
-#     pid = Enum.random(children_pids)
-#     {pid, network, next}
-#   end
-  
-#   def next(curr, network) do
-#     network = Enum.filter(network, fn pid -> Process.alive?(pid) && pid != curr end)
-#     case Enum.empty?(network) do
-#       false -> Enum.random(network)
-#       true  -> curr
-#     end
-#   end
-# end
-
-defmodule Line do
-  def create_network(numNodes) do
-    network = Enum.map(1..numNodes, fn _x -> Gossip.start_link(0) end)
-    pid = Enum.random(network)
-    IO.inspect network
-    {pid, network, &next/2}
-  end
-  
-  def next(curr, network) do
-    idx = Enum.find_index(network, fn x -> x == curr end)
-    neighbors = [idx-1, idx+1]
-              |> Enum.filter(fn idx -> idx >= 0 && idx <= length(network) - 1 end)
-              |> Enum.map(fn idx -> Enum.at(network, idx) end)
-              |> Enum.filter(fn pid -> Process.alive?(pid) end)
-    case Enum.empty?(neighbors) do
-      false -> Enum.random(neighbors)
-      true  -> curr
-    end
-  end
-end
 
 # defmodule ImperfectLine do
 #   def create_network(numNodes) do
