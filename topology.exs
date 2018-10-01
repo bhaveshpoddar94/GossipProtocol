d7ca35e6402014a986cc3405eee6b47ff15e3f18
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-10-01 14:56:15 -0400

    #4. Build a 3D grid topology.

--- topology.exs
+++ topology.exs
@@ -11,7 +11,6 @@ defmodule Topology do
   end
 end
 
-
 defmodule FullNetwork do
   def create(actor_list) do
     Enum.map(actor_list, fn pid -> {pid, Enum.filter(actor_list, fn x -> x != pid end)} end)
@@ -90,21 +89,42 @@ defmodule Line do
   end
 end
 
-# defmodule ThreeD do
-#   def create_network(numNodes) do
-#     network = Enum.map(1..numNodes, fn _x -> Gossip.start_link(0) end)
-#     pid = Enum.random(children_pids)
-#     {pid, network, next}
-#   end
-  
-#   def next(curr, network) do
-#     network = Enum.filter(network, fn pid -> Process.alive?(pid) && pid != curr end)
-#     case Enum.empty?(network) do
-#       false -> Enum.random(network)
-#       true  -> curr
-#     end
-#   end
-# end
+defmodule ThreeD do
+  def create(actor_list) do
+    n = trunc(:math.sqrt(length(actor_list)))
+    outer_loop(0, n, [], actor_list)
+    |> Enum.into(%{})
+  end
+
+  defp outer_loop(n, n, network, _actor_list), do: network
+  defp outer_loop(z, n, network, actor_list) do
+    matrix = inner_loop(z, 0, n, [], actor_list)
+    outer_loop(z+1, n, network++matrix, actor_list)
+  end
+
+  defp inner_loop(_n, n, n, matrix, _actor_list), do: matrix
+  defp inner_loop(z,  r, n, matrix, actor_list) do
+    row = Enum.map(0..n-1, fn c -> get_key_val({z, r, c}, n, actor_list) end)
+    loop(z, r+1, n, matrix++row, actor_list)
+  end
+
+  defp get_key_val({z, r, c}, n, actor_list) do
+    index = z*n*n + r*n + c
+    curr = Enum.at(actor_list, index)
+    neighbors = get_neighbors({z, r, c}, n, actor_list)
+    {curr, neighbors}
+  end
+
+  defp get_neighbors({z, r, c}, n, actor_list) do
+    [{z-1, r, c}, {z+1, r, c}, {z, r-1, c}, {z, r+1, c}, {z, r, c-1}, {z, r, c+1}]
+    |> Enum.filter(fn index -> valid_index?(index, n) end)
+    |> Enum.map(fn {z, r, c} -> Enum.at(actor_list, z*n*n + r*n + c) end)
+  end
+
+  defp valid_index?({z, r, c}, n) do
+      z >= 0 && z < n && r >= 0 && r < n && c >=0 && c < n
+  end
+end
 
 defmodule ImperfectLine do
   def create(actor_list) do
